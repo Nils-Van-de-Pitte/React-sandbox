e9338dcd7e035ea067d10a423f8127d068245b0d
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-17 14:19:33 +0200

    Create card component

---
+++ code/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+        </div>
+    );
+}
+
+export default Card
