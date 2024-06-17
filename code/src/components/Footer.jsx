56a51fff076e74a4bc8487aa81252dd2338fb76c
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-17 13:57:45 +0200

    Create footer component

---
+++ code/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            &copy; {new Date().getFullYear()} Sandboxes, All rights reserved
+        </footer>
+    );
+}
+
+export default Footer
