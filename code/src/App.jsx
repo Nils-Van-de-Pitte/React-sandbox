8a97c82568e3229fe0db9b1a82a93233d819a1c2
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-17 15:00:10 +0200

    Add card to App.jsx

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,16 +1,13 @@
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Food from "./logic/Food.jsx";
+import Card from "./components/Card.jsx";
+
 
 function App() {
-
     return(
         <>
-            <Header/>
-            <Food/>
-            <Footer/>
+            <Card/>
         </>
-    )
+    );
 }
+
 
 export default App
