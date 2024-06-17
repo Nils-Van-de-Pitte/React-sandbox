69b76cbf5e0987a16a4ddbf360099c571634763d
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-17 15:12:41 +0200

    Add external css

--- code/src/main.jsx
+++ code/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/css/card.css'
+import './assets/css/button.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
