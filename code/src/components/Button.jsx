ee3a84e94bbd3da923516a131e13d93dfff3aef4
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 16:15:02 +0200

    Add click event information

--- code/src/components/Button.jsx
+++ code/src/components/Button.jsx
@@ -2,24 +2,33 @@ import styles from '../assets/css/Button/Button.module.css'
 
 
 function Button() {
-    let count = 0
 
-    // const handleClick = () => console.log('Hello there!')
+
+    // Simple click event
+    const handleClick = () => console.log('Hello there!')
+
+    // Click event with params
     // const handleClick2 = (name) => console.log(`Hi ${name}`)
-    const handleCounter = (name) => {
-        if (count < 3){
-            count++
-            console.log(`${name}, you clicked me ${count} time/s`)
-        }else{
-            console.log(`${name}, STOP CLICKING ME!!`)
-        }
-    }
+
+    // Click event with counter
+    //let count = 0
+    // const handleCounter = (name) => {
+    //     if (count < 3){
+    //         count++
+    //         console.log(`${name}, you clicked me ${count} time/s`)
+    //     }else{
+    //         console.log(`${name}, STOP CLICKING ME!!`)
+    //     }
+    // }
+
+    // Event param
+    // const handleClick = (e) => console.log(e.target.textContent = 'OUCH!')
 
 
-
+    // e params is not necessary!
     return(
         <button className={styles.button} onClick={
-            () => handleCounter('Steve')}>
+            (e) => handleClick(e)}>
             Click me!
         </button>
     );
