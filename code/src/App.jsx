37889efd825d55c6a0241a88dd5605c7bd6f3a38
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 15:18:38 +0200

    Add second list to App.jsx

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,9 +1,22 @@
 import List from "./logic/List.jsx";
 
 function App() {
+
+    const fruits = [
+                    {id: 1, name: 'Apple', calories: 95},
+                    {id: 2, name: 'Orange', calories: 45}
+                   ]
+
+    const vegetables = [
+        {id: 1, name: 'Potatoes', calories: 110},
+        {id: 2, name: 'Carrots', calories: 15}
+    ]
+
     return(
         <>
-            <List/>
+            <List items = {fruits} category='Fruits'/>
+            <List items = {vegetables} category='Vegetables'/>
+
         </>
     );
 }
