a2730883354568b4bce965aec75f111ec5f824f6
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 13:25:41 +0200

    Apply css

--- code/src/logic/UserGreeting.jsx
+++ code/src/logic/UserGreeting.jsx
@@ -1,7 +1,9 @@
+import style from '../assets/css/Account/Account.module.css';
+
 
 function UserGreeting(props) {
     if(props.isLoggedIn){
-        return <h2>Welcome {props.name}</h2>
+        return <h2 className={style.loggedOut}>Welcome {props.name}</h2>
     }
     else{
         return <h2>Please log in to continue</h2>
