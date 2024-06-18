6dbb70777edf5e96536f2f32c859b782b312fdef
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 13:25:56 +0200

    Add to App.jsx

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,13 +1,9 @@
-import Student from "./logic/Student.jsx";
-
+import UserGreeting from "./logic/UserGreeting.jsx";
 
 function App() {
     return(
         <>
-            <Student name='Steve' age={30} isStudent={true}/>
-            <Student name='Bob' age={39} isStudent={false}/>
-            <Student/>
-
+            <UserGreeting name='Bob' isLoggedIn={true} />
         </>
     );
 }
