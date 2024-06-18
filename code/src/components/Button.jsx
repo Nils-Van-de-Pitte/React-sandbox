078052cf23207d678e3e556a7b1e03dd4df5b25a
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 16:04:40 +0200

    Add click event with params

--- code/src/components/Button.jsx
+++ code/src/components/Button.jsx
@@ -3,10 +3,12 @@ import styles from '../assets/css/Button/Button.module.css'
 
 function Button() {
 
-    const handleClick = () => console.log('Hello there!')
+    // const handleClick = () => console.log('Hello there!')
+    const handleClick2 = (name) => console.log(`Hi ${name}`)
 
     return(
-        <button className={styles.button} onClick={handleClick}>
+        <button className={styles.button} onClick={
+            () => handleClick2('Steve')}>
             Click me!
         </button>
     );
