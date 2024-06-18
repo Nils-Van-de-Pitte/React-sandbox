a5b0fddd71dc3fcef7145a36627569329a57ad7b
Nils Van de Pitte <nilsvandepitte9@gmail.com>
2024-06-18 16:08:07 +0200

    Add click event with counter

--- code/src/components/Button.jsx
+++ code/src/components/Button.jsx
@@ -2,13 +2,24 @@ import styles from '../assets/css/Button/Button.module.css'
 
 
 function Button() {
+    let count = 0
 
     // const handleClick = () => console.log('Hello there!')
-    const handleClick2 = (name) => console.log(`Hi ${name}`)
+    // const handleClick2 = (name) => console.log(`Hi ${name}`)
+    const handleCounter = (name) => {
+        if (count < 3){
+            count++
+            console.log(`${name}, you clicked me ${count} time/s`)
+        }else{
+            console.log(`${name}, STOP CLICKING ME!!`)
+        }
+    }
+
+
 
     return(
         <button className={styles.button} onClick={
-            () => handleClick2('Steve')}>
+            () => handleCounter('Steve')}>
             Click me!
         </button>
     );
